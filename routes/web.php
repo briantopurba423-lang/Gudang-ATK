3fab4f032761b9346c7aecc07c775b863725bc9a
Brianto Purba <briantopurba423@gmail.com>
2026-04-14 21:43:12 +0700

    update kode

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\TransaksiController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-use App\Http\Controllers\DashboardController;
 
-Route::get('/home', [DashboardController::class, 'index']);
+
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::get('/index', [BarangController::class, 'index']);
+
+
+
+// Barang
+Route::resource('barang', BarangController::class);
+
+// Supplier
+Route::resource('supplier', SupplierController::class);
+
+// Transaksi
+Route::post('/masuk', [TransaksiController::class, 'masuk']);
+Route::post('/keluar', [TransaksiController::class, 'keluar']);
