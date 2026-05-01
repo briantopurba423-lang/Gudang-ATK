85c67808a6cde50a7a6b66565cc12cbbcc4750a8
Brianto Purba <briantopurba423@gmail.com>
2026-05-01 23:13:00 +0700

    update kode

--- storage/framework/views/c5b83ff5f2bc2394c5687bf7a621af7f.php
+++ storage/framework/views/c5b83ff5f2bc2394c5687bf7a621af7f.php
@@ -238,7 +238,7 @@
 <footer>
     <div class="footer-logo">📦 ATK Stock System</div>
     <p>© 2026 ATK Stock System. All rights reserved.</p>
-    <p style="margin-top:10px;">Email: stokgudangatk@gmail.com | Telp: 0812-6481-0914</p>
+    <p style="margin-top:20px;">Email: stokgudangatk@gmail.com | Telp: 0812-6481-0914</p>
 </footer>
 
 </body>
